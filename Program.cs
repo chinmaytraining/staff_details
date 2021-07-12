c0a28815b780679736ccdb459b6364bb10dec16e
chinmaygireesh <chinmaygireeshs@gmail.com>
2021-07-12 21:35:11 +0530

    CODE REFACTOR:INRODUCES 3 TYPES OF STAFFS
    This commit will indruduce 3 types of staffs in the programm
    such as,
    1.Supporting staff
    2.Administartive staff
    3.Teaching staff

--- Program.cs
+++ Program.cs
@@ -6,57 +6,131 @@ namespace staff_details
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
-            staff stf = new staff();
-            Console.WriteLine("Enter the first name");
-            stf.fname = Console.ReadLine();
-            Console.WriteLine("Enter the middle name");
-            stf.mname = Console.ReadLine();
-            Console.WriteLine("Enter the  last name");
-            stf.lname = Console.ReadLine();
-            Console.WriteLine("Enter the adress");
-            stf.adress = Console.ReadLine();
-            Console.WriteLine("Enter the jobtype");
-            stf.jobtype = Console.ReadLine();
-            Console.WriteLine("Enter the age(only in numbers)");
-            stf.age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the no. of years of experinece that you have(only in numbers)");
-            stf.exp = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the salary in Rupees(only in numbers)");
-            string val= Console.ReadLine();
-            stf.salary = Convert.ToInt32(val);
-            
-            
-            stf.display_details();
-            
+            Console.WriteLine("----EMPLOYEE ENROLLING---");
+            Console.WriteLine("Select your job type \n1.Teaching staff\n2.Admin. staff\n3.Supporting Staff");
+            int ch = Convert.ToInt32(Console.ReadLine());
+            switch(ch) 
+            {
+                case 1:
+                    Teaching t = new Teaching();
+                    staf_reg(t);
+                    teaching_reg(t);
+                    staff_display(t);
+                    t.display();
+                    break;
+                case 2:
+                    Administrative a = new Administrative();
+                    staf_reg(a);
+                    administrative_reg(a);
+                    staff_display(a);
+                    a.display();
+                    break;
+
+                case 3:
+                    Supporting s = new Supporting();
+                    staf_reg(s);
+                    supporting_reg(s);
+                    staff_display(s);
+                    s.display();
+                    break;
+                default:
+                    Console.WriteLine("SELECT A VALID OPTION");
+                    break;
+            }
+
            
         }
+        public static void staf_reg(staff stf)
+        {
+            Console.WriteLine("\n\nEnter ur name");
+            stf.Name = Console.ReadLine();
+            Console.WriteLine("Enter ur Employee Id");
+            stf.EmpId = Convert.ToInt32(Console.ReadLine());
+                  
+        }
+        static void staff_display(staff stf)
+        {
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("------EPLOYEE DETAIALS------");
+            Console.WriteLine($"Employee name: {stf.Name}");
+            Console.WriteLine($"Employee id: {stf.EmpId}");
+        }
+        static void teaching_reg(Teaching stf)
+        {
+            Console.WriteLine("Enter the subject that u teach");
+            stf.Subject = Console.ReadLine();              
+        }
+        static void administrative_reg(Administrative stf)
+        {
+            Console.WriteLine("Enter ur admin. no");
+            stf.AdmId = Convert.ToInt32(Console.ReadLine());
+
+        }
+        static void supporting_reg(Supporting stf)
+        {
+            Console.WriteLine("Enter your superior name");
+            stf.Superior = Console.ReadLine();
+
+        }
+
+    
     }
     class staff
     {
-        public string fname,mname,lname;
-        public string adress;
-        public string jobtype;
-        public int age,exp,salary;
-        
-
-        public void display_details()
+        private string _name;  
+        public string Name    
         {
-            
-            string full_name = fname + ' ' +  mname +' ' + lname;
-            Console.WriteLine("");
-            Console.WriteLine("=========================");
-            Console.WriteLine($"Details about Mr.{fname}");
-            Console.WriteLine("=========================");
-            Console.WriteLine("");
-            Console.WriteLine($"full name: {full_name} ");
-            Console.WriteLine($"Adress: {adress} ");
-            Console.WriteLine($"jobtype: {jobtype} ");
-            Console.WriteLine($"Age: {age} ");
-            Console.WriteLine($"Years of experience: {exp} ");
-            Console.WriteLine($"Current salary: Rs.{salary} ");
-           
+        get => _name;
+        set => _name = value;
         }
+        private int _empid;  
+        public int EmpId    
+        {
+        get => _empid;
+        set => _empid= value;
+        }
+    }
+    class Teaching: staff
+    {
+        private string _sub;  
+        public string Subject    
+        {
+        get => _sub;
+        set => _sub = value;
+        }
+        public void display()
+        {       
+            Console.WriteLine($"Staff type: Teaching staff");
+            Console.WriteLine($"Teaching subject: {Subject}");
+        }
+    }
+    class Administrative: staff
+    { 
+        private int _admpid;  
+        public int AdmId    
+        {
+        get => _admpid;
+        set => _admpid = value;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Staff type: Administrative staff");
+            Console.WriteLine($"Administration no.:{AdmId}");
+        }
+    }
+    class Supporting: staff
+    {
+        private string _sup;  
+        public string Superior    
+        {
+        get => _sup;
+        set => _sup = value;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Staff type: Supporting staff");
+            Console.WriteLine($"Superior name:{Superior}");
+        }
+
     }
 }
